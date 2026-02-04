89275ba514775f6f25c4989ce64a3d3bf337714e
2shining <fetrafaneva@gmail.com>
2026-02-04 20:05:02 +0300

    feat: Add design components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from "./components/footer";
 import LenisScroll from "./components/lenis";
 import AboutSection from "./sections/about-section";
 import ContactSection from "./sections/contact-section";
+import DesignsSection from "./sections/design-section";
 import EducationSection from "./sections/education-section";
 import ExperienceSection from "./sections/experience-section";
 import HeroSection from "./sections/hero-section";
@@ -9,17 +10,18 @@ import ProjectsSection from "./sections/projects-section";
 import SkillsSection from "./sections/skills-section";
 
 export default function App() {
-    return (
-        <main className="max-md:px-4">
-            <LenisScroll />
-            <HeroSection />
-            <AboutSection />
-            <ProjectsSection />
-            <ExperienceSection />
-            <EducationSection />
-            <SkillsSection />
-            <ContactSection />
-            <Footer />
-        </main>
-    )
+  return (
+    <main className="max-md:px-4">
+      <LenisScroll />
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <DesignsSection />
+      <ExperienceSection />
+      <EducationSection />
+      <SkillsSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
 }
